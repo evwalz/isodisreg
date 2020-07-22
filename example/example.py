c5d6ab89e6dee3d5afe5d3c6640f8b466b4623a4
unknown <walz@mspwalz.math.kit.edu>
2020-07-22 11:01:49 +0200

    example

--- example/example.py
+++ example/example.py
@@ -1,9 +1,34 @@
-import idr
+from isodisreg import idr
 
-# Load rain data
-# Compute IDR
-# Predict
-# CDF
-# Plot
-# CRPS
+# Get data
+varNames = rain.columns[3:55]
+X = rain[varNames][0:185]
+y = rain['obs'][0:185]
+
+# Define groups and orders
+values = np.ones(52)+1
+values[0:2] = 1
+groups = dict(zip(varNames, values))
+orders = dict({"1":"comp", "2":"icx"})
+
+# compute idr
+fit = idr.idr(y = y, X = X, orders = orders, groups = groups)
+
+# fit idr / make prediction
+data = rain[varNames][185:190]
+
+preds1 = fit.predict()
+
+preds2 = fit.predict(data)
+
+
+# evaluation
+
+obs = rain['obs'][185:186]
+
+crps_values = preds2.crps(obs)
+
+eval_cdf = preds1.cdf(thresholds = 0)
+
+preds1.plot()
 
