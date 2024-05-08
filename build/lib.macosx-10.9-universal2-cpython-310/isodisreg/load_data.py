c293eb3a126f200bf4894ecbab6157596c2a395e
evwalz <evamaria.walz@gmx.de>
2024-05-08 13:03:51 +0200

    remove build

--- build/lib.macosx-10.9-universal2-cpython-310/isodisreg/load_data.py
+++
@@ -1,7 +0,0 @@
-from pkg_resources import resource_stream
-import pandas as pd
-
-def load_rain():
-    stream = resource_stream(__name__, 'packageData/rain.dat')
-    df = pd.read_csv(stream, index_col=0)
-    return(df)
