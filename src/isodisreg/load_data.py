397458809a14979535c2b95f4534981c13c3ca13
evwalz <56073009+evwalz@users.noreply.github.com>
2025-06-25 11:56:54 +0200

    Add files via upload

--- src/isodisreg/load_data.py
+++ src/isodisreg/load_data.py
@@ -1,7 +1,14 @@
-from pkg_resources import resource_stream
-import pandas as pd
+#from pkg_resources import resource_stream
 
+import pandas as pd
+try:
+    from importlib.resources import files
+except ImportError:
+    # Fallback for Python < 3.9
+    from importlib_resources import files
+    
 def load_rain():
-    stream = resource_stream(__name__, 'packageData/rain.dat')
-    df = pd.read_csv(stream, index_col=0)
-    return(df)
+    data_file = files(__name__) / 'packageData' / 'rain.dat'
+    with data_file.open('r') as stream:
+        df = pd.read_csv(stream, index_col=0)
+    return df
